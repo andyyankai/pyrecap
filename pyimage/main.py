503eb544e05bd34c6bbb223205a3066917e9ecd1
andyyankai <kyan8@uci.edu>
2018-05-26 13:43:13 -0700

    image to 2d array

---
+++ pyimage/main.py
@@ -0,0 +1,17 @@
+# transform an image to 2d array
+# currently convert all image to black and white for an easy start
+from PIL import Image
+from numpy import*
+
+temp=Image.open('test2.jpg')
+temp=temp.convert('1')      # Convert to black&white
+A = array(temp)             # Creates an array, white pixels==True and black pixels==False
+new_A=empty((A.shape[0],A.shape[1]),None)    #New array with same size as A
+
+for i in range(len(A)):
+    for j in range(len(A[i])):
+        if A[i][j]==True:
+            new_A[i][j]=0
+        else:
+            new_A[i][j]=1
+            
