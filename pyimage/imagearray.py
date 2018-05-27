60e51d91ccd73a3cd8ff99b33a5c8adf7c8ed21f
andyyankai <kyan8@uci.edu>
2018-05-26 20:17:48 -0700

    camera idea

--- pyimage/imagearray.py
+++ pyimage/imagearray.py
@@ -1,28 +1,32 @@
 # transform an image to 2d array
 # currently convert all image to black and white for an easy start
+# and do a reverse stuff
 from PIL import Image
 from numpy import*
 
 
+
+
 def image2array(x):
     temp=asarray(Image.open(x))
-    print(temp)
     x=temp.shape[0]
     y=temp.shape[1]*temp.shape[2]
 
-    temp.resize((x,y)) # a 2D array
 
     return temp
     
 def array2image(x):
-    img = Image.new('1', (64, 64))
-    #img.putdata(x)
+    img = Image.new('1', (int(size(a[0])/3), int(size(a)/size(a[0]))))
+    img = Image.fromarray(x, 'RGB')
     img.save('out.jpg')
     img.show()
 		    
 		    
 a = image2array('bigtest.jpg')
+
+print(a) 
 array2image(a)
-print(size(a)/size(a[5])) # this is the height of the image, for bigtest is 642.0
-print(size(a[5])) # this is the width of the image, for bigtest is 4800
-print(size(a[1]))
+
+print(size(a)/size(a[0])) # this is the height of the image, for bigtest is 642.0
+print(size(a[0])/3) # this is the width of the image, for bigtest is 4800
+
