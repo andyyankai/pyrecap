8c3b93711202177f2674cb9ef5bfcd515c3ee9e4
andyyankai <kyan8@uci.edu>
2018-06-02 09:40:15 -0700

    denoise and make new image for a set of image

--- opencv/imgDenoise.py
+++ opencv/imgDenoise.py
@@ -1,11 +1,17 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import glob
 
-img = cv2.imread('left01.jpg')
 
-dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
 
-plt.subplot(121),plt.imshow(img)
-plt.subplot(122),plt.imshow(dst)
-plt.show()
+images = glob.glob('*.jpg')
+
+i=1
+
+for fname in images:
+	img = cv2.imread(fname)
+	i=i+1
+	dst = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+
+	cv2.imwrite(str(i)+'.jpg',img)
