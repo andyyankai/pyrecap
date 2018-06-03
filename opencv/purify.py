deefdb42c4311e180c7b713a35a45184136944bc
andyyankai <kyan8@uci.edu>
2018-06-02 21:57:02 -0700

    purify function

--- opencv/purify.py
+++ opencv/purify.py
@@ -1,16 +1,20 @@
 from PIL import Image
 import numpy as np
 
-col = Image.open("left01.jpg")
-gray = col.convert('L')
 
-# Let numpy do the heavy lifting for converting pixels to pure black or white
-bw = np.asarray(gray).copy()
+def purify(filename):
+	col = Image.open(str(filename))
+	gray = col.convert('L')
 
-# Pixel range is 0...255, 256/2 = 128
-bw[bw < 128] = 0    # Black
-bw[bw >= 128] = 255 # White
+	# Let numpy do the heavy lifting for converting pixels to pure black or white
+	bw = np.asarray(gray).copy()
 
-# Now we put it back in Pillow/PIL land
-imfile = Image.fromarray(bw)
-imfile.save("1.jpg")
+	# Pixel range is 0...255, 256/2 = 128
+	bw[bw < 128] = 0    # Black
+	bw[bw >= 128] = 255 # White
+
+	# Now we put it back in Pillow/PIL land
+	imfile = Image.fromarray(bw)
+	imfile.save(str(filename)+"purify.jpg")
+	
+
